e53b9878149bd8f87e3ab4b8b2755646fd9873f7
Tr8ch <rak_0111@mail.ru>
2023-10-20 22:04:12 +0600

    some

--- cmd/doodocsApp/main.go
+++ cmd/doodocsApp/main.go
@@ -58,10 +58,19 @@ func setupLogger(env string) *slog.Logger {
 	case envLocal:
 		log = setupPrettySlog()
 	case envDev:
-		log = setupPrettySlog()
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
 	case envProd:
-		log = setupPrettySlog()
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
+
 	return log
 }
 
--- config/config.go
+++ config/config.go
@@ -10,7 +10,7 @@ import (
 )
 
 type Config struct {
-	Env        string `yaml:"env" env:"ENV" env-default:"local"`
+	Env        string `yaml:"env" env-default:"local"`
 	HTTPServer `yaml:"http_server"`
 }
 
